78b10b4185f428fd8a54df7696e344c7e3b337dc
Mark <mark@terinea.co.uk>
2026-06-16 15:46:05 +0100

    Add integration constants

---
+++ custom_components/talking_countdown/const.py
@@ -0,0 +1,14 @@
+DOMAIN = "talking_countdown"
+
+CONF_TTS_ENTITY = "tts_entity"
+CONF_MEDIA_PLAYER = "media_player"
+CONF_ANNOUNCEMENTS = "announcements"
+CONF_FINISHED_MESSAGE = "finished_message"
+CONF_DEFAULT_MINUTES = "default_minutes"
+
+DEFAULT_ANNOUNCEMENTS = "20,10,5,3,2,1"
+DEFAULT_FINISHED_MESSAGE = "Time is up"
+DEFAULT_MINUTES = 30
+
+SERVICE_START = "start"
+SERVICE_CANCEL = "cancel"
